3bb8e3959d17a444ad92437444c6f04efbb2dc69
yl <2336739721@qq.com>
2020-03-17 16:27:07 +0800

    1

--- yl_python_code/data-analysis/text/test.py
+++ yl_python_code/data-analysis/text/test.py
@@ -1,30 +1,5 @@
-x = 1
-y = 1
-z = 1
-
-x = y = z = 1
-
-#  x = (y = z + 1)
-
-x, y = y, x
-
-x += y
-
-x = 4.5
-
-y = 2
-
-print(x//y)
-
-x = 1
-y = 0
-z = 0
-
-if not x or y:
-    print(1)
-elif not x or not y and z:
-    print(2)
-elif not x or y or not y and x:
-    print(3)
+y = int(input("请输入年份："))
+if y % 400 == 0 or (y % 4 == 0 and y % 100 != 0):
+    print("闰年")
 else:
-    print(4)
+    print("非闰年")
